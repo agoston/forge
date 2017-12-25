b195cb37d2d61f9e5b49ad11ea5d3038b525d404
Agoston Horvath <ahorvath@bol.com>
2017-12-25 18:26:13 +0100

    added recipe. further tests w.r.t. itemblock.

--- src/main/java/org/hert/mod/BlockObsidianLilyPad.java
+++ src/main/java/org/hert/mod/BlockObsidianLilyPad.java
@@ -9,7 +9,7 @@ public class BlockObsidianLilyPad extends BlockLilyPad {
         this.setHardness(50.0F);
         this.setResistance(2000.0F);
         this.setSoundType(SoundType.STONE);
-        this.setRegistryName("hertmod", "obsidian_lilypad");
+        this.setRegistryName("hertmod", "obsidian_lilyblock");
         this.setUnlocalizedName(getRegistryName().toString());
         this.setLightLevel(0.05F);
     }
